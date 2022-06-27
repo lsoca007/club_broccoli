0870719f831a668625a76ee2d1de43f9b8d41dca
jdorta08 <dortajavier.jd2@gmail.com>
2022-06-27 14:02:14 -0400

    Cosita al css y home
    Le puse cosita al App.css y al home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,16 @@
 export default function Home() {
-    return <h1> Home </h1>
+  return (
+    <div className="hero-container">
+      <div className="column1-hero">
+        <h1>El Club al que no te deberias unir</h1>
+        <p>Nos da igual lo que hagas con tu vida, solo unete y aporta</p>
+        <button className="hero-button1">
+          Me uno porque quiero no porque me lo digas
+        </button>
+      </div>
+      <div>
+        <picture></picture>
+      </div>
+    </div>
+  );
 }
