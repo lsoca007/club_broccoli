0d63fb509b3646d118cafac861ecedc543dcb777
lsoca007 <77649258+lsoca007@users.noreply.github.com>
2022-07-01 18:29:54 -0400

    Update Contact page
    - remove demo pictures

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,4 @@
 import './style.css';
-import luis from './images/luis el crack.jpeg';
-import miguel from './images/miguel the flash.jpeg';
-import javier from './images/el pipa.jpeg';
 import 'bootstrap/dist/css/bootstrap.css'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -13,9 +10,20 @@ export default function About() {
     return (
       <div className="cards">
         <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src={luis}/>
+          <Card.Img variant="top" src={dimitri}/>
           <Card.Body>
-            <Card.Title>Luis</Card.Title>
+            <Card.Title>Development Team</Card.Title>
+            <Card.Text>
+        
+            </Card.Text>
+            <Button variant="primary">Contact</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '22rem' }}>
+          <Card.Img variant="top" src={dimitri}/>
+          <Card.Body>
+            <Card.Title>Design Team</Card.Title>
             <Card.Text>
               
             </Card.Text>
@@ -23,31 +31,21 @@ export default function About() {
           </Card.Body>
         </Card>
         <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src={miguel}/>
+          <Card.Img variant="top" src={dimitri}/>
           <Card.Body>
-            <Card.Title>Miguel</Card.Title>
+            <Card.Title>Marketing Team</Card.Title>
             <Card.Text>
-              The Selfie Man
+              
             </Card.Text>
             <Button variant="primary">Contact</Button>
           </Card.Body>
         </Card>
         <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src={javier}/>
-          <Card.Body>
-            <Card.Title>Javier</Card.Title>
-            <Card.Text>
-              The Side-Business Man
-            </Card.Text>
-            <Button variant="primary">Contact</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '25rem' }}>
           <Card.Img className = "img" variant="top" src={dimitri}/>
           <Card.Body>
             <Card.Title>Dimitri</Card.Title>
             <Card.Text>
-              Paint Houses
+              
             </Card.Text>
             <Button variant="primary">Contact</Button>
           </Card.Body>
